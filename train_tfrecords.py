1f4b73c5e953b267265aaeaf978594c86e77f84b
Maxwell Wang <jwang212@uottawa.ca>
2019-11-26 19:04:07 -0500

    continue to write

--- train_tfrecords.py
+++ train_tfrecords.py
@@ -3,6 +3,8 @@ import os
 import numpy as np 
 import model
 from network import network_definition
+import matplotlib.pyplot as plt 
+import math
 
 num_classes = 1   #WIDERFACE dataset
 image_width = 255 
@@ -21,8 +23,43 @@ def training():
     train_batch = tf.cast(train_batch, dtype=tf.float32)
     train_label_batch = tf.cast(train_label_batch, dtype=tf.int64)
 
-    train_logits = network_definition.create_network(batch_size, num_classes=None, add_logits=True, reuse=None,
-                   create_summaries=True, weight_decay=1e-8)
+    train_logits = model.inference(train_batch, batch_size, n_classes)
+    train_loss = model.losses(train_logits, train_label_batch)
+    train_op = model.training(train_loss, learning_rate)
+    train_acc = model.evaluation(train_logits, train_label_batch)
+
+    summary_op = tf.summary_merge_all()
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+    saver = tf.train.Saver()
+
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try: 
+        for step in np.arange(max_step):
+            if coord.should_stop():
+                break
+            _, tra_loss, tra_acc = sess.run([train_op, train_loss, train_acc])
+
+            if step % 50 == 0:
+                print('step %d, train loss = %.2f, train_accuracy = %.2f%%' %(step, training, tra_acc*100.0))
+                summary_str = sess.run(summary_op)
+                train_writer.add_summary(summary_str, step)
+
+            if step % 2000 == 0 or (step + 1) == max_step:
+                checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+    
+    except tf.errors.OutOfRangeError:
+        print('Done training -- epoch limit reached')
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
+    sess.close()
+
 
 
 
