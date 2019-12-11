6d226c321979f40e47e0e761f29c370409fb5442
Maxwell Wang <jwang212@uottawa.ca>
2019-12-11 17:25:58 -0500

    convert to tfrecord

--- model.py
+++ model.py
@@ -7,16 +7,19 @@ import os
 import random
 from tqdm import tqdm 
 import numpy as np  
+import pandas as pd 
+import sys
 
+sys.path.append('/home/max/Downloads/MTCNN/models/research/')
+from PIL import Image
+from object_detection.utils import dataset_util # from path
+from collections import namedtuple, OrderedDict # tf slim
 
-output_train_path = '/home/maxwell/Downloads/MTCNN/training_feature_descriptor/image_set'
+output_path = '/home/max/Downloads/MTCNN/training_feature_descriptor/tfrecord'
 
-if not os.path.exists(output_train_path):
-    os.makedirs(output_train_path)
+input_image_list = '/home/max/Downloads/cosine_metric_learning/datasets/test_csv/'
 
-
-MODES = [tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL, tf.estimator.ModeKeys.PREDICT]
-
+class_label = ['face']
 
 def _float_feature(value):
     if not isinstance(value, list):
@@ -33,15 +36,31 @@ def _bytes_feature(value):
         value = [value]
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
+def get_training_images(input_image_list):
+    files = []
+    #input_image_list = '/home/max/Downloads/cosine_metric_learning/datasets/test_csv/'
+    for filename in sorted(os.listdir(input_image_list)):
+        image_list_path = os.path.join(input_image_list, filename)
+        #print(image_list_path)
 
-def convert_to_tfrecord(mode, anno):
+        for line in open(image_list_path, "r"):
+            data = line.split(",")
+            image = data[0]
+            filename_path = os.path.join(input_image_list,image)
+            print(filename_path)
+            files.append(filename_path)
 
-    assert mode in MODES, "wrong tfrecord"
+    return files 
 
-    filename = os.path.join(FLAGS.save_dir, mode + '.tfrecords')
 
-    with tf.python_io.TFRecordWriter(filename) as writer:
-        for fnm, classes in tqdm(anno):
+def convert_to_tfrecord(output_path, anno):
+
+    
+    image_path = get_training_images(input_image_list)
+
+    with tf.python_io.TFRecordWriter(image_path) as writer:
+
+        for fnm, classes in anno:
 
             # read and convert
             img = io.imread(fnm)
@@ -57,8 +76,8 @@ def convert_to_tfrecord(mode, anno):
             example = tf.train.Example(
                 features=tf.train.Features(
                     feature={
-                        'image/height': _int_feature(rows),
-                        'image/width': _int_feature(cols),
+                        'image/height': _int_feature(height),
+                        'image/width': _int_feature(width),
                         'image/depth': _int_feature(depth),
                         'image/class/label': _int_feature(classes),
                         'image/encoded': _bytes_feature(img.astype(np.float32).tobytes())
@@ -67,26 +86,30 @@ def convert_to_tfrecord(mode, anno):
                 )
             )
             writer.write(example.SerializeToString())
-
-
-def get_training_images(input_image_list):
-    #input_image_list = '/home/max/Downloads/cosine_metric_learning/datasets/test_csv/'
-    for filename in sorted(os.listdir(input_image_list)):
-        image_list_path = os.path.join(input_image_list, filename)
-        #print(image_list_path)
-
-        for line in open(image_list_path, "r"):
-            data = line.split(",")
-            image = data[0]
-            filename_path = os.path.join(input_image_list,image)
-            print(filename_path)
+     
 
 
 def get_annotations(directory, classes):
-    files = []
-    labels = []
+    
+    files = get_training_images(directory)
 
-    for 
+    labels = []
+    for i in range(len(files)):
+        labels.append(classes)
+    
+    annotation = [x for x in zip(files, labels)]
+
+    return annotation
+
+def main(_):
+
+    annotation = get_annotations(input_image_list, class_label)
+
+    convert_to_tfrecord(output_path, annotation)
+
+if __name__ == '__main__':
+    tf.app.run()
+
 
 
 
